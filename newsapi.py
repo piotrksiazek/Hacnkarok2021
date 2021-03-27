d1069ea51159c1defd0d43ec3dd29e2f917864bd
Piotr Ksiazek <pksiazek.py@gmail.com>
2021-03-27 21:57:51 +0100

    deploy 5

--- newsapi.py
+++ newsapi.py
@@ -7,9 +7,9 @@ class NewsApi:
     def __init__(self):
         self.api_key = '26db25030c8a456aa45c9577b89bfd4b'
 
-    def get_date(self):
+    def get_date(self, days):
         today = str(datetime.today()).split()[0]
-        yesterday = today[:-2] + str(int(today[-2:]) - 1)
+        yesterday = today[:-2] + str(int(today[-2:]) - days)
         return [yesterday, today]
 
     def get_headlines(self):
@@ -19,10 +19,10 @@ class NewsApi:
         return titles
 
     def get_about(self, topic):
-        date = self.get_date()
+        date = self.get_date(7)
         yesterday = date[0]
         today = date[1]
         response = json.loads(
-            requests.get(f'https://newsapi.org/v2/everything?q={topic}&from={yesterday}&to={today}&sortBy=popularity&apiKey={self.api_key}').content)
+            requests.get(f'https://newsapi.org/v2/top-headlines?q={topic}&from={yesterday}&to={today}&sortBy=popularity&apiKey={self.api_key}').content)
         titles = [article['title'] for article in response['articles']]
         return titles
