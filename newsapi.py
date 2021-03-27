f52afab84a5dee4ff1f8b4acdf37ab4cb67fa4e9
Piotr Ksiazek <pksiazek.py@gmail.com>
2021-03-27 19:33:10 +0100

    newsapi

--- newsapi.py
+++ newsapi.py
@@ -1,5 +1,7 @@
 import requests
 import json
+from datetime import datetime
+
 
 class NewsApi:
     def __init__(self):
@@ -10,3 +12,7 @@ class NewsApi:
         articles = response['articles']
         titles = [article['title'] for article in articles]
         return titles
+
+    # def get_about(self, topic):
+    #     response = json.loads(
+    #         requests.get(f'https://newsapi.org/v2/everything?q={topic}&from=2021-03-26&to=2021-03-26&sortBy=popularity&apiKey={self.api_key}'))
