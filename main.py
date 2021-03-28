c0f72922fb32e4c7fa23ae9e4942733a82586579
Piotr Ksiazek <pksiazek.py@gmail.com>
2021-03-28 01:26:44 +0100

    zdjecia

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 from scraper import Scraper
 from newsapi import NewsApi
+from picture import pixelate
 from selenium import webdriver
 app = Flask(__name__)
 scraper = Scraper()
@@ -13,18 +14,19 @@ def world_population():
         'worldometers': scraper.get_worldometers(),
         'news_api': news_api.get_headlines(),
         'healthcare_pandemic': {
-            'healthcare': news_api.get_about('healthcare'),
-            'pandemic': news_api.get_about('pandemic')
+            'healthcare': news_api.get_about('healthcare', 0)[0],
+            'pandemic': news_api.get_about('pandemic', 0)[0]
         },
         'environment_climate_change': {
-            'environment': news_api.get_about('environment'),
-            'climate_change': news_api.get_about('climate')
+            'environment': news_api.get_about('environment', 0)[0],
+            'climate_change': news_api.get_about('climate', 0)[0]
         },
         'politics_protests_finance': {
-            'politics': news_api.get_about('politics'),
-            'protests': news_api.get_about('protests'),
-            'finance': news_api.get_about('finance')
-        }
+            'politics': news_api.get_about('politics', 0)[0],
+            'protests': news_api.get_about('protests', 0)[0],
+            'finance': news_api.get_about('finance', 0)[0]
+        },
+        'picture_urls': news_api.get_about('politics', 1)[1]
     }
     return jsonify(response)
 
--- newsapi.py
+++ newsapi.py
@@ -1,11 +1,11 @@
 import requests
 import json
 from datetime import datetime
-
+from picture import get_image_from_url, pixelate
 
 class NewsApi:
     def __init__(self):
-        self.api_key = '80ecc87904c345b199ba7c3978bc4377'
+        self.api_key = '595a3f8e15834a11af60d2babb04c086'
 
     def get_date(self, days):
         today = str(datetime.today()).split()[0]
@@ -18,11 +18,28 @@ class NewsApi:
         titles = [article['title'] for article in articles]
         return titles
 
-    def get_about(self, topic):
+    def get_about(self, topic, with_pictures):
         date = self.get_date(7)
         yesterday = date[0]
         today = date[1]
         response = json.loads(
             requests.get(f'https://newsapi.org/v2/top-headlines?q={topic}&from={yesterday}&to={today}&sortBy=popularity&apiKey={self.api_key}').content)
+        titles = []
+        picture_urls = []
+        processed_images = []
+        for article in response['articles']:
+            titles.append(article['title'])
+            picture_urls.append(article['urlToImage'])
+
+        if with_pictures:
+            for i, url in enumerate(picture_urls):
+                if url:
+                    try:
+                        name = get_image_from_url(url, i)
+                        pixelate(name, f'static/{name}', 5)
+                        picture_urls.append(f'static/{name}')
+                    except requests.exceptions.MissingSchema:
+                        continue
+            
         titles = [article['title'] for article in response['articles']]
-        return titles
+        return titles, picture_urls
